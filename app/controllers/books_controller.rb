e9f2188246ee00747fd31c6638eb60fbeb4aebba
suzuki <s.suzukitime0211@gmail.com>
2021-05-10 14:29:35 +0000

    (update)5/10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,42 @@
 class BooksController < ApplicationController
   def index
    @books = Book.all
-   @books = Book.new
-   book = Book.new(book_params)
-   book.save
-   redirect_to books_path
+   @book = Book.new
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def new
+  
   end
+  
+  def create
+   book = Book.new(book_params)
+   book.save
+   redirect_to book_path(book.id)
+  end
+  
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path 
+   end
 
   def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+    blog = Book.find(params[:id])
+    blog.update(book_params)
+    redirect_to book_path(book)
   end
   
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
